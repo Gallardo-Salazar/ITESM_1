53f110eafab243c25e6e58213d09c31154e6996e
user <gallardoirg@gmail.com>
2024-06-05 17:11:13 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,83 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 26143720,
+    "boundingBox": {
+        "lx": 472578.9686787077,
+        "ly": 2140273.2039605726,
+        "lz": 2543.759735118092,
+        "ux": 473076.02787290609,
+        "uy": 2140770.2631547709,
+        "uz": 3040.818929316436
+    },
+    "tightBoundingBox": {
+        "lx": 472578.9686564022,
+        "ly": 2140273.2039576715,
+        "lz": 2543.7597507168916,
+        "ux": 472947.63205640218,
+        "uy": 2140770.2631576715,
+        "uz": 2619.580550716892
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 4.304658889770508,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
